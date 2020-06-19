1dd2ba642b2068c7b64475350c3e4d3f3d646586
Zamar Zahid <zamerzahid@gmail.com>
2020-06-19 19:05:22 +0500

    first commit

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Contacts } from '../contacts/Contacts';
+
+export const Home = () => {
+  return (
+    <div className="grid-2">
+      <div>{/* Contact Form */}</div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
+};
